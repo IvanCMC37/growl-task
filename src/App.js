4e59ed9669ae10013aecf3f4ba8060961d6626a1
IvanCMC37 <iot3534199@gmail.com>
2022-03-11 12:38:37 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from './logo.svg'
+import './App.css'
+import { useGrowl, Growl } from './growl'
+
+function App() {
+	const [active, setActive] = useGrowl()
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<Link className="App-link" to="/other">
+					Go to the Other component
+        		</Link>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+        		</p>
+				<a className="App-link" href="#" onClick={() => void setActive(true)}>
+					Clik here to activate the growl
+        		</a>
+				{/* <select onChange={(e) =>{const selectedOption=e.target.value;
+				setAutoActive(selectedOption)}}>
+					<option value ="true">yes</option>
+					<option value ="false">no</option>
+				</select> */}
+				
+			</header>
+			<Growl onDismissed={() => setActive(false)} active={active} message="Hello World!" />
+		</div>
+	)
+}
+
+export default App;
